ea81dfac18b46c791a2a68bdbb18970e61a0220d
GitHub Action <action@github.com>
2025-09-23 10:05:52 +0000

    Update build_tarballs.jl version, Project.toml version, and Artifacts.toml with new tarball URLs

--- iceberg_rust_ffi_binarybuilder/build_tarballs.jl
+++ iceberg_rust_ffi_binarybuilder/build_tarballs.jl
@@ -1,10 +1,10 @@
 using BinaryBuilder
 
 name = "iceberg_rust_ffi"
-version = v"0.1.0"
+version = v"0.2.9"
 
 sources = [
-    GitSource("https://github.com/RelationalAI/iceberg_rust_ffi.git", "583c8f2c2cc5cb5f92100ad8eae32f22b7a76c09"),
+    GitSource("https://github.com/RelationalAI/iceberg_rust_ffi.git", "f1cf6e51bcf84e26016114e28c7d8a5a825a3ae5"),
 ]
 
 # Bash recipe for building across all platforms
